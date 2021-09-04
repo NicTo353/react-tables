2522a7f2e19850f0bf17e02cd716d4cdeb6f0f39
BrenevAlexey <brenevalexey@yandex.ru>
2021-09-04 18:10:13 +0300

    request works

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const RecordsTable = () => {
+  const [tableData, setTableData] = React.useState(['name']);
+
+  const url = "https://jsonplaceholder.typicode.com/users";
+
+
+  React.useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        return setTableData(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return <div>{console.log(tableData)}</div>;
+};
+
+ReactDOM.render(<RecordsTable />, document.getElementById("root"));
