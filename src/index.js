2294f388fb98e29a02f4b336b3e42e45b0e21308
BrenevAlexey <brenevalexey@yandex.ru>
2021-09-04 20:54:16 +0300

    Table component added

--- src/index.js
+++ src/index.js
@@ -3,24 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import axios from 'axios';
 
+const Table = ({props}) => {
+
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {Object.keys(props[0]).map((header, index) => {
+            return <td key={index}>{header}</td>
+          })}
+        </tr>
+      </thead>
+      <tbody>
+        {
+          props.map(row => {
+            return <tr key={row.id}></tr>
+          })
+        }
+      </tbody>
+    </table>
+  )
+}
+
+
 
 const RecordsTable = () => {
-  const [tableData, setTableData] = React.useState(['name']);
+  const [tableData, setTableData] = React.useState();
 
   const url = "https://jsonplaceholder.typicode.com/users";
 
 
   React.useEffect(() => {
     axios
-      .get(url)
-      .then((res) => {
-        return setTableData(res.data);
-      })
+      .get(url) 
+      .then(res => res.data)
+      .then(data => setTableData(data))
       .catch((error) => {
         console.log(error);
       });
   }, []);
-  return <div>{console.log(tableData)}</div>;
+
+
+  return (
+    <div>{tableData ? (<Table props={tableData}></Table>) : ('Loading...')}</div>
+  );
 };
 
 ReactDOM.render(<RecordsTable />, document.getElementById("root"));
